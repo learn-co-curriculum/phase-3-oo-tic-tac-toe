07b8a53513a1b8b4ec81c91193f14e6b0bd4497c
Noah A Klem <noahaklem@gmail.com>
2023-09-13 11:17:33 -0600

    TicTacToe initialize board

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,16 @@
+class TicTacToe
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ]
+    def initialize
+        @board = Array.new(9, " ")
+    end
+
+end
