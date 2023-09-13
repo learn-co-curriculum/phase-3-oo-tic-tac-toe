207b1f8b2ae1e2175b5e8ba38b84d39ade0f7aaa
Noah A Klem <noahaklem@gmail.com>
2023-09-13 11:36:20 -0600

    helper methods

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,4 +13,36 @@ class TicTacToe
         @board = Array.new(9, " ")
     end
 
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index(input)
+        input = input.to_i - 1
+    end
+
+    def move(index, token = "X")
+        @board[index] = token
+    end
+
+    def position_taken?(index)
+        @board[index] != " "
+    end
+
+    def valid_move?(index)
+        !position_taken?(index) && index.between?(0,9)
+    end
+
+    def turn_count
+        @board.count {|token| token != " "}
+    end
+
+    def current_player
+        turn_count % 2 == 0 ? "X" : "O"
+    end
+
 end
